bc1eabda02a909aacdef2f59aac6605b3b8adef9
ertuozdenli <ertu.ozdenli@gmail.com>
2023-10-12 11:11:12 +0300

    multi layout

--- src/components/icons/IconCheck.vue
+++ src/components/icons/IconCheck.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 const checked = toRef(props, 'checked')
 </script>
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
     <g fill="#000000">
       <path
         v-if="checked"
